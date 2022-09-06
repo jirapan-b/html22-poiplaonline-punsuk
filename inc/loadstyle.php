cbbc94dff057c7f3e4028bd080a2da14e24679b3
Ohm <jeraphan2000@gmail.com>
2022-09-07 04:59:46 +0700

    php to html

--- inc/loadstyle.php
+++
@@ -1,8 +0,0 @@
-<!-- Core -->
-<link rel="stylesheet" href="assets/css/plugin/bootstrap.min.css">
-<link rel="stylesheet" href="assets/css/plugin/normalize.min.css">
-<link rel="stylesheet" href="assets/css/plugin/animate.min.css">
-
-<!-- Custom -->
-<?php $modify="?v=".date('Ymd').time(); ?>
-<link rel="stylesheet" type="text/css" href="assets/css/style.css<?=$modify?>">
